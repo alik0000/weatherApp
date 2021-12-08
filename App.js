6f4818d029216680406b4dfc4e63743b89157f21
Alijon Kurbanov <alijon.kurbanov.99@mail.ru>
2021-12-08 22:57:42 +0700

    changed app file

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import { Alert } from 'react-native';
+import {Alert, StyleSheet, View, Image, Text} from 'react-native';
 import * as Location from 'expo-location'
 import axios from "axios";
 import {Weather} from "./components/Weather";
 
+
 export default function App() {
     const [temperature, setTemp] = useState()
     const [condition, setCondition] = useState()
+    const [date, setDate] = useState()
+    const [time, setTime] = useState()
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     const API_KEY = 'cdfaa876bc03b9b449de745419ea06a7';
     async function getWeather(lat, lon) {
@@ -29,10 +35,73 @@ export default function App() {
                 Alert.alert('Error', 'Some Error!')
             }
         })();
+
+        setInterval(()=> {
+            let today = new Date()
+            const month = today.getMonth()
+            const day = today.getDay()
+            const date = today.getDate()
+            const hour = today.getHours()
+            const minutes = today.getMinutes()
+            const hoursFormat = hour >= 13 ? hour % 12: hour
+            const meridiem = hour >= 12 ? 'pm' : 'am'
+            setTime((hoursFormat < 10? '0'+hoursFormat : hoursFormat) + ':' + (minutes < 10? '0'+minutes: minutes) + meridiem)
+            setDate(days[day] + ', ' + date+ ' ' + months[month])
+        }, 1000)
+
+
     }, [])
+    const url = () => {
+        const hours = new Date().getHours()
+        return (
+            hours >= 5 && hours <= 11? require(`./assets/morning.jpg`) :
+                hours >= 12 && hours <= 16? require(`./assets/afternoon.jpg`) :
+                    hours >= 17 && hours <= 23? require(`./assets/evening.jpg`) :
+                        hours >= 24 && hours <= 4? require(`./assets/night.jpg`) : ''
+        )
+    }
     return (
-        <Weather temp={temperature} condition={condition}/>
+        <View style={styles.container}>
+            <Image source={url()} style={styles.ImgStyle}/>
+            <View style={styles.Date}>
+                <View>
+                    <Text style={styles.timeStyle}>{time}</Text>
+                </View>
+                <View>
+                    <Text style={styles.dateStyle}>{date}</Text>
+                </View>
+            </View>
+            <Weather temp={temperature} condition={condition}/>
+        </View>
     )
-
-
 }
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFill,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ImgStyle: {
+        ...StyleSheet.absoluteFill,
+        width: '100%',
+        height: '100%',
+    },
+    Date: {
+        position: "absolute",
+        fontSize: 30,
+        top: 100,
+        textAlign: "center",
+    },
+    timeStyle: {
+        fontSize: 40,
+        fontWeight: '500',
+        textAlign: "center",
+        color: 'white',
+    },
+    dateStyle: {
+        fontSize: 25,
+        color: '#eee',
+        fontWeight: '300'
+    }
+});
--- components/Weather.js
+++ components/Weather.js
@@ -1,52 +1,55 @@
 import React from "react";
-import { StyleSheet, View, Text, StatusBar } from "react-native";
-import {globalStyle} from "../styles/global";
+import { StyleSheet, Text, StatusBar } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import {LinearGradient} from "expo-linear-gradient";
 
 export const Weather = ({temp, condition}) => {
     return (
-        <LinearGradient
-            colors={weatherOptions[condition].colors}
-            style={globalStyle.container}>
-            <View style={styles.wrapper}>
-                <Ionicons name={weatherOptions[condition].icon} size={66} color="#f2fcfe"/>
-                <Text style={styles.text}>
-                    The temperature is: {temp}°{'\n'}
-                    The Weather is: {condition}.{'\n'}
-                </Text>
-            </View>
+        <LinearGradient colors={weatherOptions[condition].colors} style={styles.content}>
+            <Ionicons name={weatherOptions[condition].icon} size={40} color="#f2fcfe"/>
+            <Text style={styles.text}>
+                The temperature is: {temp}°{'\n'}
+                The Weather is: {condition}.{'\n'}
+            </Text>
             <StatusBar barStyle="light-content"/>
         </LinearGradient>
     )
 }
 const styles = StyleSheet.create({
     text: {
-        fontSize: 25,
+        fontSize: 16,
         color: 'white',
         textAlign: 'center',
+        paddingTop: 10,
     },
-    wrapper: {
-        flex: 1,
+    content: {
+        position: "absolute",
+        width: 300,
+        height: 150,
+        top: 200,
+        borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+        shadowColor: "#000000",
+        shadowRadius: 60.00,
+        elevation: 24,
+    },
 })
 const weatherOptions = {
     Rain: {
         icon: 'rainy',
-        colors: ['#00F260', '#0575E6']
+        colors: ['#00F26089', '#0575E689'],
     },
     Snow: {
         icon: 'snow',
-        colors: ['#1c92d2', '#f2fcfe']
+        colors: ['#1C92D291', '#F2FCFEA0'],
     },
     Clear: {
-        icon:'weather-sunny',
-        colors: ['#EDE574', '#E1F5C4', '#78ffd6']
+        icon:'sunny',
+        colors: ['#EDE57487', '#E1F5C491', '#78FFD693'],
     },
     Clouds: {
         icon: 'cloud',
-        colors: ['#4b4b4b', '#FFFFFF']
-    }
+        colors: ['#4B4B4B99', '#FFFFFFA3'],
+    },
 }
